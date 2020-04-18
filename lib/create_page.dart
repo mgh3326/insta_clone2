72c3ce61c69c873b5512e6d1fb5ad7dc5f4d8548
robin <mgh3326@naver.com>
2020-04-18 17:48:59 +0900

    게시물 작성

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -33,7 +35,11 @@ class _CreatePageState extends State<CreatePage> {
 
   // 갤러리에서 사진 가져오기
   Future _getImage() async {
-
+    var image = await ImagePicker.pickImage(
+        source: ImageSource.gallery, maxWidth: 640, maxHeight: 480);
+    setState(() {
+      _image = image;
+    });
   }
 
   @override
@@ -60,15 +66,25 @@ class _CreatePageState extends State<CreatePage> {
 
   Future _uploadFile(BuildContext context) async {
     // 스토리지에 업로드할 파일 경로
-
+    final fireBaseStorageRef = FirebaseStorage.instance
+        .ref()
+        .child('post')
+        .child('${DateTime.now().millisecondsSinceEpoch}.png');
     // 파일 업로드
-
+    final task = fireBaseStorageRef.putFile(
+        _image, StorageMetadata(contentType: 'image/png'));
     // 완료까지 기다림
-
+    final storageTestSnapshot = await task.onComplete;
     // 업로드 완료 후 url
-
+    final downloadUrl = await storageTestSnapshot.ref.getDownloadURL();
     // 문서 작성
-
+    await Firestore.instance.collection('post').add({
+      'content': textEditingController.text,
+      'displayName': widget.user.displayName,
+      'email': widget.user.email,
+      'photoUrl': downloadUrl,
+      'userPhotoUrl': widget.user.photoUrl
+    }); 
     // 완료 후 앞 화면으로 이동
     Navigator.pop(context);
   }
